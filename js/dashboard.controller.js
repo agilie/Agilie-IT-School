73f4f41f7a6ad5c5d198cf88e0a5d530d0eac7c0
SergeyMell <sergey.mell@agilie.com>
2017-06-22 18:05:02 +0300

    Our firsst controller and nested scopes

---
+++ js/dashboard.controller.js
@@ -0,0 +1,15 @@
+;(function(){
+    angular
+        .module("myApp")
+        .controller("DashboardCtrl", function($scope) {
+
+            $scope.message = 'Hello World';
+
+            $scope.todos = {
+                current: ['Task1', 'Task2', 'Task3'],
+                delayed: ['Task4', 'Task5', 'Task6', 'Task7', 'Task8'],
+                urgent: ['Task9', 'Task10', 'Task11', 'Task12']
+            };
+
+        });
+})();
