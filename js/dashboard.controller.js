6b0fa99da937873c770672971f69585eaeed00c4
SergeyMell <sergey.mell@agilie.com>
2017-06-23 11:27:41 +0300

    Scope apply

--- js/dashboard.controller.js
+++ js/dashboard.controller.js
@@ -4,14 +4,40 @@
 
     angular
         .module("myApp")
-        .controller("DashboardCtrl", function ($scope) {
 
-            $scope.$watch('userEmail', function (newName, oldName) {
-                console.log('user is typing his email');
-            });
+        .controller("ScopeWithoutApply", function ($scope) {
 
-            $scope.$watchGroup(['userName', 'userEmail'], function (newValues, oldValues) {
-                console.log('user is typing something', newValues, oldValues);
-            });
+            $scope.getMessage = function () {
+                setTimeout(function () {
+                    $scope.message = 'Fetched after 3 seconds';
+                    console.log($scope.message);
+                }, 3000);
+            };
+
+            $scope.getMessage();
+        })
+
+        .controller("ScopeWithApply", function ($scope) {
+            $scope.getMessage = function () {
+                setTimeout(function () {
+                    $scope.$apply(function () {
+                        $scope.message = 'Fetched after 2 seconds';
+                        console.log($scope.message);
+                    });
+                }, 2000);
+            };
+
+            $scope.getMessage();
+        })
+
+        .controller("CorrectWay", function ($scope, $timeout) {
+            $scope.getMessage = function () {
+                $timeout(function () {
+                    $scope.message = 'Fetched after 1 second';
+                    console.log($scope.message);
+                }, 1000);
+            };
+
+            $scope.getMessage();
         });
 })();
